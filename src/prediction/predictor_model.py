2333c92aeec258b267eee3e27a4f4c0e10deb093
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-12-16 23:24:53 +0200

    refactor

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -27,8 +27,11 @@ class Forecaster:
     def __init__(
         self,
         data_schema: ForecastingSchema,
+        history_forecast_ratio: int = None,
+        lags_forecast_ratio: int = None,
         alpha: Optional[float] = 1.0,
         lags: Union[int, List[int]] = 20,
+        use_exogenous: bool = True,
         random_state: int = 0,
     ):
         """Construct a new Ridge Forecaster
@@ -37,6 +40,16 @@ class Forecaster:
 
             data_schema (ForecastingSchema): Schema of the data used for training.
 
+            history_forecast_ratio (int):
+                Sets the history length depending on the forecast horizon.
+                For example, if the forecast horizon is 20 and the history_forecast_ratio is 10,
+                history length will be 20*10 = 200 samples.
+
+            lags_forecast_ratio (int):
+                Sets the lags parameters depending on the forecast horizon.
+                lags = forecast horizon * lags_forecast_ratio
+                This parameters overides lags parameters.
+
             alpha (Optional[int]): Constant that multiplies the L2 term, controlling regularization strength. alpha must be a non-negative float i.e. in [0, inf).
                 When alpha = 0, the objective is equivalent to ordinary least squares, solved by the LinearRegression object. For numerical reasons, using alpha = 0 with the Ridge object is not advised.
                 Instead, you should use the LinearRegression object.
@@ -44,6 +57,9 @@ class Forecaster:
             lags (Union[int, List[int]]): Lags used as predictors. Index starts at 1, so lag 1 is equal to t-1.
                 - int: include lags from 1 to lags (included).
                 - list, 1d numpy ndarray or range: include only lags present in lags, all elements must be int.
+
+            use_exogenous (bool):
+                If true, uses covariates in training.
 
             random_state (int): Sets the underlying random seed at model initialization time.
         """
@@ -54,23 +70,73 @@ class Forecaster:
         self.models = {}
         self.data_schema = data_schema
         self.end_index = {}
+        self.history_length = None
+        self.use_exogenous = use_exogenous
+
+        if history_forecast_ratio:
+            self.history_length = (
+                self.data_schema.forecast_length * history_forecast_ratio
+            )
+        if lags_forecast_ratio:
+            lags = self.data_schema.forecast_length * lags_forecast_ratio
+            self.lags = lags
+
+    def _add_future_covariates_from_date(
+        self,
+        history: pd.DataFrame,
+        data_schema: ForecastingSchema,
+        is_training: bool = True,
+    ) -> pd.DataFrame:
+        """
+        Creates future covariates from the data column.
+
+        Args:
+            history (pd.DataFrame):
+                The data to create the covariates on.
+
+            data_schema (ForecastingSchema):
+                Schema of the data.
+
+            is_training (bool):
+                Set to true if the process is to be done on the training data.
+
+        Returns (pd.DataFrame): The processed dataframe.
+        """
+
+        future_covariates_names = data_schema.future_covariates
+        if data_schema.time_col_dtype in ["DATE", "DATETIME"]:
+            date_col = pd.to_datetime(history[data_schema.time_col])
+            year_col = date_col.dt.year
+            month_col = date_col.dt.month
+            year_col_name = f"{data_schema.time_col}_year"
+            month_col_name = f"{data_schema.time_col}_month"
+            history[year_col_name] = year_col
+            history[month_col_name] = month_col
+            if is_training:
+                future_covariates_names += [year_col_name, month_col_name]
+
+        return history, future_covariates_names
 
     def fit(
         self,
         history: pd.DataFrame,
         data_schema: ForecastingSchema,
-        history_length: int = None,
     ) -> None:
         """Fit the Forecaster to the training data.
-        A separate Ridge model is fit to each series that is contained
+        A separate RandomForest model is fit to each series that is contained
         in the data.
 
         Args:
             history (pandas.DataFrame): The features of the training data.
             data_schema (ForecastingSchema): The schema of the training data.
-            history_length (int): The length of the series used for training.
         """
         np.random.seed(self.random_state)
+        (
+            history,
+            future_covariates,
+        ) = self._add_future_covariates_from_date(
+            history=history, data_schema=data_schema, is_training=True
+        )
         groups_by_ids = history.groupby(data_schema.id_col)
         all_ids = list(groups_by_ids.groups.keys())
         all_series = [
@@ -81,9 +147,14 @@ class Forecaster:
         self.models = {}
 
         for id, series in zip(all_ids, all_series):
-            if history_length:
-                series = series[-history_length:]
-            model = self._fit_on_series(history=series, data_schema=data_schema, id=id)
+            if self.history_length:
+                series = series[-self.history_length :]
+            model = self._fit_on_series(
+                history=series,
+                data_schema=data_schema,
+                id=id,
+                future_covariates=future_covariates,
+            )
             self.models[id] = model
 
         self.all_ids = all_ids
@@ -91,19 +162,26 @@ class Forecaster:
         self.data_schema = data_schema
 
     def _fit_on_series(
-        self, history: pd.DataFrame, data_schema: ForecastingSchema, id: int
+        self,
+        history: pd.DataFrame,
+        data_schema: ForecastingSchema,
+        id: int,
+        future_covariates: List = None,
     ):
-        """Fit Ridge model to given individual series of data"""
-        model = Ridge(alpha=self.alpha, random_state=self.random_state)
+        """Fit RandomForest model to given individual series of data"""
+        model = Ridge(
+            alpha=self.alpha,
+            random_state=self.random_state,
+        )
         forecaster = ForecasterAutoreg(regressor=model, lags=self.lags)
 
-        covariates = data_schema.future_covariates
+        covariates = future_covariates
 
         history.index = pd.RangeIndex(start=0, stop=len(history))
 
         self.end_index[id] = len(history)
         exog = None
-        if covariates:
+        if covariates and self.use_exogenous:
             exog = history[covariates]
 
         forecaster.fit(y=history[data_schema.target], exog=exog)
@@ -122,6 +200,10 @@ class Forecaster:
         if not self._is_trained:
             raise NotFittedError("Model is not fitted yet.")
 
+        test_data, future_covariates = self._add_future_covariates_from_date(
+            history=test_data, data_schema=self.data_schema, is_training=False
+        )
+
         groups_by_ids = test_data.groupby(self.data_schema.id_col)
         all_series = [
             groups_by_ids.get_group(id_).drop(columns=self.data_schema.id_col)
@@ -131,7 +213,9 @@ class Forecaster:
         all_forecasts = []
         for id_, series_df in zip(self.all_ids, all_series):
             forecast = self._predict_on_series(
-                key_and_future_df=(id_, series_df), id=id_
+                key_and_future_df=(id_, series_df),
+                id=id_,
+                future_covariates=future_covariates,
             )
             forecast.insert(0, self.data_schema.id_col, id_)
             all_forecasts.append(forecast)
@@ -144,15 +228,15 @@ class Forecaster:
         )
         return all_forecasts
 
-    def _predict_on_series(self, key_and_future_df, id):
+    def _predict_on_series(self, key_and_future_df, id, future_covariates):
         """Make forecast on given individual series of data"""
         key, future_df = key_and_future_df
 
         start = self.end_index[id]
         future_df.index = pd.RangeIndex(start=start, stop=start + len(future_df))
         exog = None
-        covariates = self.data_schema.future_covariates
-        if covariates:
+        covariates = future_covariates
+        if covariates and self.use_exogenous:
             exog = future_df[covariates]
 
         if self.models.get(key) is not None:
@@ -211,18 +295,12 @@ def train_predictor_model(
     Returns:
         'Forecaster': The Forecaster model
     """
-    history_length = None
-    history_forecast_ratio = hyperparameters.get("history_forecast_ratio")
-    if history_forecast_ratio:
-        history_length = data_schema.forecast_length * history_forecast_ratio
-    if "history_forecast_ratio" in hyperparameters:
-        hyperparameters.pop("history_forecast_ratio")
 
     model = Forecaster(
         data_schema=data_schema,
         **hyperparameters,
     )
-    model.fit(history=history, data_schema=data_schema, history_length=history_length)
+    model.fit(history=history, data_schema=data_schema)
     return model
 
 
